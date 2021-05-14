76600e8a9b6a612be0e25703dfffc62ec8bae651
MorozovGeeK <morozov4811@gmail.com>
2021-05-14 18:19:43 +0400

    Initial commit

---
+++ src/test/java/ru/geekbrains/AMorozov/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.AMorozov.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
